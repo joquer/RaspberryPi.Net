edf752efe1e5240eaddfb75138d7095c7cd921bc
Michael Glasgow <glasgowm@advisory.com>
2015-05-24 21:37:49 -0500

    Added in the rest of the GPIO files and Build Framework files. May need to do much more customizing of these files, since .Net 5.0 uses dnu instead of nuget to create nuget packages,

---
+++ RaspberryPi.GPIO/GPIODirection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RaspberryPi.GPIO
+{
+    /// <summary>
+    /// Specifies the direction of the GPIO port
+    /// </summary>
+    public enum GPIODirection { In, Out };
+}
---
+++ RaspberryPi.GPIO/PinState.cs
@@ -0,0 +1,11 @@
+﻿namespace RaspberryPi.GPIO
+{
+    /// <summary>
+    /// This is a cleaner way to write the Pin Status
+    /// </summary>
+    public enum PinState
+    {
+        High,
+        Low
+    }
+}
