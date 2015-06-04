8f3c60745eccf0c6741acde92df73ab99833814b
Michael Glasgow <glasgowm@advisory.com>
2015-06-03 23:05:55 -0500

    Added a project with a simple command line program to test things.

---
+++ RaspberryPi.Sensor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RaspberryPi.Sensor
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("RaspberryPi.Sensor Starting...");
+            Console.WriteLine("RaspberryPi.Sensor Exiting...");
+        }
+    }
+}
